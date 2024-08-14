708e5cee829ef7cba4f9cf18313a60b9588ba763
kieranp-p6 <kiip96@icloud.com>
2024-08-14 16:43:51 +0100

    kjdbnfibaicbaionc

--- app/page.js
+++ app/page.js
@@ -1,24 +1,16 @@
 import Link from 'next/link';
 import Main from "./src/components/main/Main"
-//import { useState } from 'react';
+import { useState } from 'react';
 
 
 
 export default function Home() {
-  /*
-    const [menuStatus, SetMenuStatus] = useState(false);
-  
-    function handleClick() {
-      SetMenuStatus(!menuStatus)
-    }
-  */
   return (
     <>
       <Link href="founders">Got to founders page</Link>
       <main className="siteMain">
         <Main />
       </main>
-
     </>
   );
 }
--- app/src/components/header/Header.jsx
+++ app/src/components/header/Header.jsx
@@ -1,7 +1,18 @@
+"use client";
+
+import { useState } from "react";
+
 const Header = () => {
+  const [menuStatus, SetMenuStatus] = useState(false);
+
+  function handleClick() {
+    SetMenuStatus(!menuStatus);
+    console.log("button clicks");
+  }
   return (
     <header className="siteHeader">
       <h3>🔥Fireplace Palace</h3>
+      <button onClick={handleClick}>Menu toggle</button>
     </header>
   );
 };
