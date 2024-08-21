9a6a9909ad641fe71cae49820ea7842c0f5410e1
Maria <maria.g1611@yahoo.com>
2024-08-21 14:18:51 +0100

    done the city and house part of the useReducer function along with some commits

--- app/booking/contactForm/ContactForm.js
+++ app/booking/contactForm/ContactForm.js
@@ -3,10 +3,13 @@ import styles from "./ContactForm.module.css"
 import { useReducer } from "react";
 import React from "react";
 
+//setting the initialState
 const initialState = {
     data: {
         fullName: "",
         postcode: "",
+        house: "",
+        city: "",
     },
     error: false,
 }
@@ -15,9 +18,12 @@ function reducer(state, action) {
     switch (action.type) {
         case 'SET_FIELD':
             return {
+                //here it is spreading the state. Copies the stuff in initialState as its own state (as an object) 
+                //and then when you ask for data it gives you the data in the copied state.
                 ...state,
                 data: {
                     ...state.data,
+                    //this code goes into the 
                     [action.payload.field]: action.payload.value
                 }
             };
@@ -40,15 +46,17 @@ const ContactForm = () => {
     const [state, dispatch] = useReducer(reducer, initialState);
     console.log(state)
     function handleClick(e) {
+        //prevents constant refresh of form
         e.preventDefault();
-        if (!state.data.fullName || !state.data.postcode) {
+        //if not {object} then show error otherwise no error shown
+        if (!state.data.fullName || !state.data.postcode || !state.data.house || !state.data.city) {
             dispatch({ type: 'SHOW_ERROR' });
         } else {
             dispatch({ type: 'CLEAR_ERROR' });
 
         }
     }
-
+//function for the dispatch aka the action part. what is inputted in the CASE return part of reducer function
     function handleChange(e) {
         dispatch({
             type: 'SET_FIELD',
@@ -80,7 +88,19 @@ const ContactForm = () => {
                         name="postcode"
                         onChange={handleChange} />
                     {/* house address input */}
+                    <label className={styles.label} htmlFor="house">House/Flat Number and Street Name*</label>
+                    <input className={styles.input}
+                        type="text" 
+                        value={state.data.house}
+                        name="house"
+                         onChange={handleChange} />
                     {/* city input */}
+                    <label className={styles.label} htmlFor="city">City*</label>
+                    <input className={styles.input} 
+                         type="text" 
+                        value={state.data.city} 
+                         name="city"
+                        onChange={handleChange} />
                 </fieldset>
 
                 {/* new filedset with email and phone number */}
