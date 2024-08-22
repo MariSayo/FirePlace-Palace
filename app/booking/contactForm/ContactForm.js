179b6ac88fa104a9cf5ecbd95b9aa3dc3cfdc275
kieranp-p6 <kiip96@icloud.com>
2024-08-22 11:03:57 +0100

    refactored everything

--- app/booking/contactForm/ContactForm.js
+++ app/booking/contactForm/ContactForm.js
@@ -1,9 +1,10 @@
-'use client'
-import styles from "./ContactForm.module.css"
+'use client';
 import { useReducer } from "react";
 import React from "react";
+import Form from './form';
+import styles from './ContactForm.module.css'
 
-//setting the initialState
+// Setting the initialState
 const initialState = {
     data: {
         fullName: "",
@@ -16,49 +17,45 @@ const initialState = {
     loadingState: false,
     successState: false,
     error: false,
-}
+};
 
 function reducer(state, action) {
     switch (action.type) {
         case 'SET_FIELD':
             return {
-                //here it is spreading the state. Copies the stuff in initialState as its own state (as an object) 
-                //and then when you ask for data it gives you the data in the copied state.
                 ...state,
                 data: {
                     ...state.data,
-                    //this code goes into the 
                     [action.payload.field]: action.payload.value
                 }
-            }
+            };
         case 'SHOW_ERROR':
             return {
                 ...state,
                 error: true
-            }
+            };
         case 'CLEAR_ERROR':
             return {
                 ...state,
                 error: false
-            }
+            };
         case 'RESET_FIELDS':
-            return { ...initialState }
+            return {
+                ...initialState,
+                successState: state.successState, // Preserve successState on reset
+            };
         case 'BUTTON_SWITCH':
+            console.log('Button Switch Triggered');
             return {
                 ...state,
-                data: {
-                    ...state.data
-                },
                 loadingState: true,
-            }
+            };
         case 'SUCCESS_TOGGLE':
             return {
                 ...state,
-                data: {
-                    ...state.data
-                },
+                loadingState: false,  // Reset loading state after success
                 successState: true,
-            }
+            };
         default:
             return state;
     }
@@ -66,107 +63,33 @@ function reducer(state, action) {
 
 const ContactForm = () => {
     const [state, dispatch] = useReducer(reducer, initialState);
-    function handleClick(e) {
-        //prevents constant refresh of form
+
+    function handleSubmit(e) {
+        // Prevents constant refresh of form
         e.preventDefault();
-        //if not {object} then show error otherwise no error shown
-        if (!state.data.fullName || !state.data.postcode || !state.data.house || !state.data.city) {
+
+        // Start loading and clear previous error
+        dispatch({ type: 'CLEAR_ERROR' });
+
+
+        // If required fields are not filled, show an error
+        if (!state.data.fullName || !state.data.postcode || !state.data.house || !state.data.city || !state.data.number || !state.data.number) {
             dispatch({ type: 'SHOW_ERROR' });
         } else {
-            dispatch({ type: 'CLEAR_ERROR' });
-            dispatch({ type: 'BUTTON_SWITCH' });
-
+            dispatch({ type: 'BUTTON_SWITCH' }); // change loading state to true (changing text to loading)
             setTimeout(() => {
-                dispatch({ type: 'RESET_FIELDS' });
+                dispatch({ type: 'RESET_FIELDS' }); // resets fields
                 dispatch({ type: 'SUCCESS_TOGGLE' });
-            }, 2000)
+                console.log(state);
+            }, 2000);
         }
-    }
-
-
-    //function for the dispatch aka the action part. what is inputted in the CASE return part of reducer function
-    function handleChange(e) {
-        dispatch({
-            type: 'SET_FIELD',
-            payload: {
-                field: e.target.name,
-                value: e.target.value
-            }
-        });
     }
 
     return (
         <>
-            {!state.successState ? <form className={styles.form} onSubmit={handleClick}>
-                <fieldset className={styles.personalInfo}>
-                    <legend className={styles.legend}>Personal Information</legend>
-                    {/* fullName input */}
-                    <label className={styles.label} htmlFor="fullName">Full name*</label>
-                    <input className={styles.input}
-                        type="text"
-                        value={state.data.fullName}
-                        name="fullName"
-                        onChange={handleChange} />
-
-                    {/* postcode input */}
-                    <label className={styles.label} htmlFor="postcode">Postcode*</label>
-                    <input className={styles.input}
-                        type="text"
-                        value={state.data.postcode}
-                        name="postcode"
-                        onChange={handleChange} />
-                    {/* house address input */}
-                    <label className={styles.label} htmlFor="house">House/Flat Number and Street Name*</label>
-                    <input className={styles.input}
-                        type="text"
-                        value={state.data.house}
-                        name="house"
-                        onChange={handleChange} />
-                    {/* city input */}
-                    <label className={styles.label} htmlFor="city">City*</label>
-                    <input className={styles.input}
-                        type="text"
-                        value={state.data.city}
-                        name="city"
-                        onChange={handleChange} />
-                </fieldset>
-
-                {/* new filedset with email and phone number */}
-
-                <fieldset className={styles.contactInfo}>
-                    <legend className={styles.legend}>Contact Information</legend>
-                    <label className={styles.label} htmlFor="number">Phone number*</label>
-                    <input className={styles.input}
-                        type="tel"
-                        value={state.data.number}
-                        name="number"
-                        onChange={handleChange} />
-
-                    <label className={styles.label} htmlFor="email">Email Address*</label>
-                    <input className={styles.input}
-                        type="email"
-                        value={state.data.email}
-                        name="email"
-                        onChange={handleChange} />
-                </fieldset>
-
-                {state.error && (
-                    <p className={styles.error}>Error: All fields are required - some are missing.</p>
-                )}
-
-
-
-                <button type="submit" className={styles.submit}>
-                    {!state.loadingState ? "Request Design Consultation" : "Loading..."}
-                </button>
-            </form>
-                :
-                <div className={styles.successMessage}>
-                    <h2>SUCCESSSSSSSSSSSSS</h2>
-                </div>
-            }
+            {state.successState ? <p className={styles.successMessage}>Success!!!!!</p> : <Form state={state} dispatch={dispatch} onSubmit={handleSubmit} />}
         </>
     );
-}
+};
 
 export default ContactForm;
---
+++ app/booking/contactForm/form.js
@@ -0,0 +1,71 @@
+import styles from './ContactForm.module.css';
+
+const Form = ({ state, dispatch, onSubmit }) => {
+    const formOptions = [
+        { Labelfor: "fullName", Labelcontent: "Full Name*", inputType: "text", inputValue: state.data.fullName, inputName: "fullName" },
+        { Labelfor: "postcode", Labelcontent: "Postcode*", inputType: "text", inputValue: state.data.postcode, inputName: "postcode" },
+        { Labelfor: "house", Labelcontent: "House/Flat Number and street name*", inputType: "text", inputValue: state.data.house, inputName: "house" },
+        { Labelfor: "city", Labelcontent: "City*", inputType: "text", inputValue: state.data.city, inputName: "city" },
+    ];
+
+    const extraFormOptions = [
+        { Labelfor: "number", Labelcontent: "Tel number*", inputType: "tel", inputValue: state.data.number, inputName: "number" },
+        { Labelfor: "email", Labelcontent: "Email*", inputType: "email", inputValue: state.data.email, inputName: "email" },
+    ];
+
+    function handleChange(e) {
+        dispatch({
+            type: 'SET_FIELD',
+            payload: {
+                field: e.target.name,
+                value: e.target.value
+            }
+        });
+    }
+
+    return (
+        <>
+            {/*  fieldset with fullname, postcode, address and city */}
+            <form className={styles.form} onSubmit={onSubmit}>
+                <fieldset className={styles.personalInfo}>
+                    <legend className={styles.legend}>Personal Information</legend>
+                    {formOptions.map((formInput) => (
+                        <div key={formInput.inputName} className={styles.inputGroup}>
+                            <label className={styles.label} htmlFor={formInput.Labelfor}>{formInput.Labelcontent}</label>
+                            <input
+                                className={styles.input}
+                                type={formInput.inputType}
+                                value={formInput.inputValue}
+                                name={formInput.inputName}
+                                onChange={handleChange}
+                            />
+                        </div>
+                    ))}
+                </fieldset>
+
+                {/*  fieldset with email and phone number */}
+                <fieldset className={styles.contactInfo}>
+                    <legend className={styles.legend}>Contact Information</legend>
+                    {extraFormOptions.map((formInput) => (
+                        <div key={formInput.inputName} className={styles.inputGroup}>
+                            <label className={styles.label} htmlFor={formInput.Labelfor}>{formInput.Labelcontent}</label>
+                            <input
+                                className={styles.input}
+                                type={formInput.inputType}
+                                value={formInput.inputValue}
+                                name={formInput.inputName}
+                                onChange={handleChange}
+                            />
+                        </div>
+                    ))}
+                </fieldset>
+                <p className={styles.error}>{state.error ? "Fill out all the forms" : ""}</p>
+                <button type="submit" className={styles.submit}>
+                    {state.loadingState ? "Loading..." : "Request Design Consultation"}
+                </button>
+            </form>
+        </>
+    );
+};
+
+export default Form;
