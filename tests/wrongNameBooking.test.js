7580dba8a76a2e24203d5596255d8c85db2a265a
kieranp-p6 <kiip96@icloud.com>
2024-08-23 12:05:52 +0100

    user jouney e2e testing

--- tests/wrongNameBooking.test.js
+++ tests/wrongNameBooking.test.js
@@ -3,7 +3,6 @@ import { test, expect } from '@playwright/test'
 test('testing name input', async ({ page }) => {
   await page.goto('http://localhost:3000/')
   await page.getByRole('link', { name: 'Here' }).click()
-  await page.getByRole('link', { name: 'Here' }).click()
 
   // expect to go to booking page
   // Wait for navigation to complete
@@ -11,13 +10,15 @@ test('testing name input', async ({ page }) => {
 
   // Assert that the current URL is the booking page
   expect(page.url()).toBe('http://localhost:3000/booking')
+  // click the fullname input field
   await page.locator('input[name="fullName"]').click()
-  await page.locator('input[name="fullName"]').fill('kieran paget')
-
+  //fill it with incorrect data
+  await page.locator('input[name="fullName"]').fill('kieran')
+  // submit
   await page
     .getByRole('button', { name: 'Request Design Consultation' })
     .click()
-
+  // expect error to be thrown
   await expect(
     page.locator('p', {
       hasText:
