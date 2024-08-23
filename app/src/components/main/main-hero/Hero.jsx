a8adb0a3913ff1a978c9681df2d2e10b57a6bcaa
kieranp-p6 <kiip96@icloud.com>
2024-08-23 08:44:50 +0100

    fixed hero typo

--- app/src/components/main/main-hero/Hero.jsx
+++ app/src/components/main/main-hero/Hero.jsx
@@ -1,29 +1,30 @@
 import styles from './Hero.module.css'
 import Image from 'next/image'
-import Link from "next/link";
+import Link from 'next/link'
 
 const Hero = () => {
-    return (
-        <section className={styles.heroWrapper}>
-            <div className={styles.heroImage}>
-                <Image
-                    src='/hero-mobile.png'
-                    alt='hero mobile'
-                    priority={true}
-                    width='500'
-                    height='500'
-                />
-            </div>
-            <div className={styles.heroText}>
-                <h1>Discover the Perfect Fireplace...</h1>
-                <h3>
-                    Book consultation:
-                    <span className={styles.colourChange}><Link href="/booking"
-                    >Book consultation</Link></span>
-                </h3>
-            </div>
-        </section>
-    )
+  return (
+    <section className={styles.heroWrapper}>
+      <div className={styles.heroImage}>
+        <Image
+          src='/hero-mobile.png'
+          alt='hero mobile'
+          priority={true}
+          width='500'
+          height='500'
+        />
+      </div>
+      <div className={styles.heroText}>
+        <h1>Discover the Perfect Fireplace...</h1>
+        <h3>
+          Request a call back
+          <span className={styles.colourChange}>
+            <Link href='/booking'> Here</Link>
+          </span>
+        </h3>
+      </div>
+    </section>
+  )
 }
 
 export default Hero
